cb97e9a2bd34db1f1f68ab336205b48634b8ad74
Daniel Núñez <42550783+JumpNShootMan@users.noreply.github.com>
2019-10-04 13:37:01 -0500

    Add files via upload
    ejercicio 1 del simulacro de EA

---
+++ Ej1.py
@@ -0,0 +1,38 @@
+import time
+import os
+
+file = open("entrada.txt","r")
+maxima = int(file.readline().strip())
+print(maxima)
+nitems = int(file.readline().strip())
+v = [0 for x in range(nitems)]
+w = [0 for x in range(nitems)]
+for i in range(nitems):
+    w[i], v[i] = [int(x) for x in file.readline().split()]
+
+global pesoMax, valMax
+pesoMax = 0
+valMax = 0
+
+def FBMochila(n, peso, val):
+    global pesoMax, valMax
+    if n < nitems:
+        FBMochila(n+1, peso+w[n], val+v[n])
+        FBMochila(n+1, peso, val)
+    if val>valMax and peso < maxima:
+        valMax = val
+        pesoMax = peso
+
+start= time.time()
+FBMochila(0,0,0)
+print(pesoMax,valMax)
+
+file2 = open("salida.txt","w")
+file2.write("El peso es de: " + str(pesoMax) + " kg"+os.linesep)
+file2.write("El valor es de: " + str(valMax) + " soles"+os.linesep)
+
+file2.close()
+
+end = time.time()
+
+print("tiempo: ", end-start," segundos")
